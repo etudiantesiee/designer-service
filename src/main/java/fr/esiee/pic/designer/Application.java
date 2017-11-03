13f15bf9b52e1143a18dcf8e06c85a074d986b40
lti <landry.tiehi@gmail.com>
2017-11-03 18:11:33 +0100

    add mvn structure

---
+++ src/main/java/fr/esiee/pic/designer/Application.java
@@ -0,0 +1,27 @@
+package fr.esiee.pic.designer;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Demarrage de l'application
+ * @author etudiant
+ *
+ */
+@SpringBootApplication
+public class Application {
+ 
+    static final Logger logger = LogManager.getLogger(Application.class.getName());
+    
+    /**
+     * Main class
+     */
+    public static void main(String[] args) {
+        logger.info("entered application");
+        
+        SpringApplication.run(Application.class, args);
+    }
+}
