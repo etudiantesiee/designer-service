a33840ca9f94939e0995b6166722ab1ff16809e2
AN.DENIS <denis.an@edu.esiee.fr>
2018-12-19 16:44:11 +0100

    yeux ceinture poche

--- src/main/java/fr/esiee/pic/designer/design/evaluation/DessinGroupe6.java
+++ src/main/java/fr/esiee/pic/designer/design/evaluation/DessinGroupe6.java
@@ -21,6 +21,8 @@ public class DessinGroupe6 extends CreateurDeForme {
 	 */
 	@Override
 	public void dessiner() {
+		
+		//tete
 		Point p1 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 45, EcranEvaluation.UNITE_VERTICALE_GRILLE * 20);
 		Point p2 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 20);
 		Point p3 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 46, EcranEvaluation.UNITE_VERTICALE_GRILLE * 21);
@@ -38,7 +40,7 @@ public class DessinGroupe6 extends CreateurDeForme {
 		Point p15 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 41, EcranEvaluation.UNITE_VERTICALE_GRILLE * 23);
 		Point p16 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 42, EcranEvaluation.UNITE_VERTICALE_GRILLE * 21);
 		Point p17 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 45, EcranEvaluation.UNITE_VERTICALE_GRILLE * 20);
-		
+		//corps
 		Point p18 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 43, EcranEvaluation.UNITE_VERTICALE_GRILLE * 28);
 		Point p19 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 42, EcranEvaluation.UNITE_VERTICALE_GRILLE * 30);
 		Point p20 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 41, EcranEvaluation.UNITE_VERTICALE_GRILLE * 31);
@@ -57,7 +59,41 @@ public class DessinGroupe6 extends CreateurDeForme {
 		Point p33 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 53, EcranEvaluation.UNITE_VERTICALE_GRILLE * 31);
 		Point p34 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 52, EcranEvaluation.UNITE_VERTICALE_GRILLE * 30);
 		Point p35 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 51, EcranEvaluation.UNITE_VERTICALE_GRILLE * 28);
+		//ceinture
+		Point p36 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 43, EcranEvaluation.UNITE_VERTICALE_GRILLE * 32);
+		Point p37 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 51, EcranEvaluation.UNITE_VERTICALE_GRILLE * 32);
+		//bouche
+		Point p38 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 45, EcranEvaluation.UNITE_VERTICALE_GRILLE * 28);
+		Point p39 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 49, EcranEvaluation.UNITE_VERTICALE_GRILLE * 28);
+		//poche
+		Point p40 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 49, EcranEvaluation.UNITE_VERTICALE_GRILLE * 30.6);
+		Point p41 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 49, EcranEvaluation.UNITE_VERTICALE_GRILLE * 31);
+		Point p42 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 50, EcranEvaluation.UNITE_VERTICALE_GRILLE * 31);
+		Point p43 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 50, EcranEvaluation.UNITE_VERTICALE_GRILLE * 30.6);
 		
+		//oeil G
+		Point p44 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 24);
+		Point p45 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 25);
+		Point p46 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 46, EcranEvaluation.UNITE_VERTICALE_GRILLE * 26);
+		Point p47 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 43, EcranEvaluation.UNITE_VERTICALE_GRILLE * 26);
+		Point p48 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 42, EcranEvaluation.UNITE_VERTICALE_GRILLE * 25);
+		Point p49 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 42, EcranEvaluation.UNITE_VERTICALE_GRILLE * 23);
+		Point p50 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 43, EcranEvaluation.UNITE_VERTICALE_GRILLE * 22);
+		Point p51 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 46, EcranEvaluation.UNITE_VERTICALE_GRILLE * 22);
+		Point p52 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 23);
+		Point p53 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 24);
+		
+		//oeil G
+		Point p54 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 52, EcranEvaluation.UNITE_VERTICALE_GRILLE * 24);
+		Point p55 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 52, EcranEvaluation.UNITE_VERTICALE_GRILLE * 25);
+		Point p56 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 51, EcranEvaluation.UNITE_VERTICALE_GRILLE * 26);
+		Point p57 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 48, EcranEvaluation.UNITE_VERTICALE_GRILLE * 26);
+		Point p58 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 25);
+		Point p59 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 47, EcranEvaluation.UNITE_VERTICALE_GRILLE * 23);
+		Point p60 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 48, EcranEvaluation.UNITE_VERTICALE_GRILLE * 22);
+		Point p61 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 51, EcranEvaluation.UNITE_VERTICALE_GRILLE * 22);
+		Point p62 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 52, EcranEvaluation.UNITE_VERTICALE_GRILLE * 23);
+		Point p63 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 52, EcranEvaluation.UNITE_VERTICALE_GRILLE * 24);
 		
 		demarrerNouveauDessinAvecDesPoints()
 		.ajouter(p1).nePasRelierLesPointsExtreme()
@@ -76,7 +112,9 @@ public class DessinGroupe6 extends CreateurDeForme {
 		.ajouter(p14)
 		.ajouter(p15)
 		.ajouter(p16)
-		.ajouter(p17)
+		.ajouter(p17);
+		
+		demarrerNouveauDessinAvecDesPoints()
 		.ajouter(p18).nePasRelierLesPointsExtreme()
 		.ajouter(p19)
 		.ajouter(p20)
@@ -96,6 +134,43 @@ public class DessinGroupe6 extends CreateurDeForme {
 		.ajouter(p34)
 		.ajouter(p35).nePasRelierLesPointsExtreme();
 		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p36).nePasRelierLesPointsExtreme()
+		.ajouter(p37).nePasRelierLesPointsExtreme();
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p38).nePasRelierLesPointsExtreme()
+		.ajouter(p39).nePasRelierLesPointsExtreme();
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p40).nePasRelierLesPointsExtreme()
+		.ajouter(p41)
+		.ajouter(p42)
+		.ajouter(p43).nePasRelierLesPointsExtreme();
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p44).nePasRelierLesPointsExtreme()
+		.ajouter(p45)
+		.ajouter(p46)
+		.ajouter(p47)
+		.ajouter(p48)
+		.ajouter(p49)
+		.ajouter(p50)
+		.ajouter(p51)
+		.ajouter(p52)
+		.ajouter(p53).nePasRelierLesPointsExtreme();
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p54).nePasRelierLesPointsExtreme()
+		.ajouter(p55)
+		.ajouter(p56)
+		.ajouter(p57)
+		.ajouter(p58)
+		.ajouter(p59)
+		.ajouter(p60)
+		.ajouter(p61)
+		.ajouter(p62)
+		.ajouter(p63).nePasRelierLesPointsExtreme();
 	}
 
 }
