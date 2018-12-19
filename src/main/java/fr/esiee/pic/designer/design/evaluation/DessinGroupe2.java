7d9bd0f4ef6fa595b99ba7924ae7c8bb8633e6e5
lamia.marwa <lamia.marwa@mail.fr>
2018-12-19 17:13:28 +0100

    Menton, pupilles, couleur gr2

--- src/main/java/fr/esiee/pic/designer/design/evaluation/DessinGroupe2.java
+++ src/main/java/fr/esiee/pic/designer/design/evaluation/DessinGroupe2.java
@@ -1,5 +1,7 @@
 package fr.esiee.pic.designer.design.evaluation;
 
+import fr.esiee.pic.designer.domain.shapes.Couleur;
+import fr.esiee.pic.designer.domain.shapes.Ellipse;
 import fr.esiee.pic.designer.domain.shapes.Point;
 import fr.esiee.pic.designer.domain.tools.CreateurDeForme;
 import fr.esiee.pic.designer.ui.scene.evaluation.EcranEvaluation;
@@ -85,7 +87,8 @@ public class DessinGroupe2 extends CreateurDeForme {
 		.ajouter(p26)
 		.ajouter(p27)
 		.ajouter(p28)
-		.ajouter(p29);
+		.ajouter(p29)
+		.couleurDeFond(Couleur.GRIS);
 		
 		//Points yeux
 		Point p30 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 3 + decalage, EcranEvaluation.UNITE_VERTICALE_GRILLE * 7);
@@ -114,7 +117,8 @@ public class DessinGroupe2 extends CreateurDeForme {
 		.ajouter(p38)
 		.ajouter(p39)
 		.ajouter(p40)
-		.ajouter(p41);
+		.ajouter(p41)
+		.couleurDeFond(Couleur.GRIS);
 		
 		//Points nez
 		Point p42 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 6.7 + decalage, EcranEvaluation.UNITE_VERTICALE_GRILLE * 8.3);
@@ -145,6 +149,30 @@ public class DessinGroupe2 extends CreateurDeForme {
 		demarrerNouveauDessinAvecDesPoints()
 		.ajouter(p47)
 		.ajouter(p48);
+		
+		//Points menton
+		Point p49 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 6 + decalage, EcranEvaluation.UNITE_VERTICALE_GRILLE * 14);
+		Point p50 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 8 + decalage, EcranEvaluation.UNITE_VERTICALE_GRILLE * 14);
+		
+		//Dessin menton
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p10)
+		.ajouter(p50)
+		.ajouter(p49)
+		.ajouter(p22)
+		.nePasRelierLesPointsExtreme();
+		
+		//Pupille gauche
+		Point p51 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 5 + decalage, EcranEvaluation.UNITE_VERTICALE_GRILLE * 7);
+		
+		Ellipse e1 = new Ellipse("gauche", p51, 10, 10, Couleur.NOIR);
+		ajouterEllipse(e1);
+		
+		//Pupille droite
+		Point p52 = new Point(EcranEvaluation.UNITE_HORIZONTALE_GRILLE * 9 + decalage, EcranEvaluation.UNITE_VERTICALE_GRILLE * 7);
+		Ellipse e2 = new Ellipse("droite", p52, 10, 10, Couleur.NOIR);
+		ajouterEllipse(e2);
+		
 
 		
 	}
