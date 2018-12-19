be00ee4d39d63395b397fced61a34562f7e988ee
AN.DENIS <denis.an@edu.esiee.fr>
2018-12-19 15:53:37 +0100

    Contour de tete 12

--- src/main/java/fr/esiee/pic/designer/ui/scene/evaluation/EcranEvaluation.java
+++ src/main/java/fr/esiee/pic/designer/ui/scene/evaluation/EcranEvaluation.java
@@ -1,6 +1,7 @@
 package fr.esiee.pic.designer.ui.scene.evaluation;
 
 import fr.esiee.pic.designer.design.evaluation.DessinGroupe0;
+import fr.esiee.pic.designer.design.evaluation.DessinGroupe6;
 import fr.esiee.pic.designer.ui.scene.common.Ecran;
 
 public class EcranEvaluation extends Ecran {
@@ -8,12 +9,12 @@ public class EcranEvaluation extends Ecran {
     /**
      * Unite sur l'axe des abscissed
      */
-    public static final int UNITE_HORIZONTALE_GRILLE = 50;
+    public static final int UNITE_HORIZONTALE_GRILLE = 10;
 
     /**
      * Unité sur l'axe des ordonnées
      */
-    public static final int UNITE_VERTICALE_GRILLE = 50;
+    public static final int UNITE_VERTICALE_GRILLE = 10;
     
     @Override
     public int uniteHorizontaleGrille() {
@@ -36,8 +37,9 @@ public class EcranEvaluation extends Ecran {
         
         // Affichage de la démo : personnage manga
         DessinGroupe0 demoManga = new DessinGroupe0("Dessin groupe 0");
+        DessinGroupe6 demoManga6 = new DessinGroupe6("Dessin groupe 6");
         e.addDessin(demoManga);
-        
+        e.addDessin(demoManga6);
         e.afficher();
     }
 }
